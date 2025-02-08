866a76facad2e877716d36ba5deab8789a792395
Prerana-kumbhar <kumbharprerana20@gmail.com>
2025-02-08 06:34:38 +0000

    update

--- calculator.py
+++ calculator.py
@@ -1,9 +1,9 @@
 # Simple Python Calculator
 def add(x, y):
     return x + y
-#comment by userA
-#def sub(x,y):
-#    return x-y
+comment by userA
+def sub(x,y):
+    return x-y
 
 def multiply(x, y):
     return x * y
